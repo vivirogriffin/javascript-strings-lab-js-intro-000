f8405c94578f793d11992686f0aab34724d163ed
Vivien Griffin <vivirogriffin@gmail.com>
2021-03-02 21:13:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "anyone other than rapey Neil deGrasse Tyson";
 
-var greetSpecialGuest = "Hello, everybody" + specialGuest + "!";
+var greetSpecialGuest = "Hello" + specialGuest + "?";
 
 var topic = "space";
 
