46c965a60be490a48974bb448394380ff25cff0d
Vivien Griffin <vivirogriffin@gmail.com>
2021-03-02 21:17:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Buzz Aldrin";
 
-var greetSpecialGuest = "Hello" + specialGuest + "?";
+var greetSpecialGuest = "Hello " + specialGuest + "!";
 
 var topic = "space";
 
